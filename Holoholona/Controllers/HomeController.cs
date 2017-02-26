a5a0d4182103777f32bd1bfa73866954db33bdc9
Peter <peter.html@gmail.com>
2017-02-26 19:28:49 +0100

    Tidy up some serverside code

--- Holoholona/Controllers/HomeController.cs
+++ Holoholona/Controllers/HomeController.cs
@@ -1,15 +1,10 @@
 ﻿using Holoholona.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Holoholona.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();

3470874a55cdff6a47395e44958b6acee396272c
Peter <peter.html@gmail.com>
2017-02-26 20:12:50 +0100

    Add a ViewModel to contain Mammals (for now) and some code structure

--- Holoholona/Controllers/HomeController.cs
+++ Holoholona/Controllers/HomeController.cs
@@ -14,7 +14,18 @@ namespace Holoholona.Controllers
         public ActionResult GetDog()
         {
             Mammal dog = new Dog { Name = "Chihuahua", Type = "Dog" };
+            
             return Json(dog, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetMammals()
+        {
+            Mammal dog = new Dog { Name = "Chihuahua", Type = "Dog" };
+            Mammal cat = new Cat { Name = "Siameser", Type = "Cat" };
+            GetMammalsViewModel Mammals = new GetMammalsViewModel { Dog = dog, Cat = cat };
+
+            return Json(Mammals, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Holoholona/Models/GetMammalsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Holoholona.Models
+{
+    public class GetMammalsViewModel
+    {
+        public Mammal Dog { get; set; }
+        public Mammal Cat { get; set; }
+    }
+}

51a1a2445dd16dc39ba039cae24ac75b2909e6c4
Morten Andersen <morten_liebmann@hotmail.com>
2017-03-11 22:37:58 +0100

    Added a Controller to return Json files and added bootstrap

---
+++ Holoholona/Controllers/JsonController.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Holoholona.Web.Controllers
+{
+    public class JsonController : Controller
+    {
+        // GET: Json
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public object GetGameStructure()
+        {
+            //JObject o1 = JObject.Parse(File.ReadAllText(Server.MapPath("~/scripts/gameStructure.json")));
+            //// read JSON directly from a file
+            //using (StreamReader file = File.OpenText(Server.MapPath("~/scripts/gameStructure.json")))
+            //using (JsonTextReader reader = new JsonTextReader(file))
+            //{
+            //    JObject o2 = (JObject)JToken.ReadFrom(reader);
+            //    return Json(o2, JsonRequestBehavior.AllowGet);
+            //}
+
+            string allText = System.IO.File.ReadAllText(Server.MapPath("~/scripts/gameStructure.json"));
+
+            object jsonObject = JsonConvert.DeserializeObject(allText);
+            return jsonObject;
+        }
+    }
+}

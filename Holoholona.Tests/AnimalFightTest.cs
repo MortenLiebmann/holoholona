3ccf370301928a1e8037c53bc53e9fdc9ffe0670
Morten Andersen <morten_liebmann@hotmail.com>
2017-03-11 23:06:24 +0100

    Added test for Animal fights

---
+++ Holoholona.Tests/AnimalFightTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Holoholona.Web.Models;
+
+namespace Holoholona.Tests
+{
+    [TestClass]
+    public class AnimalFightTest
+    {
+        [TestMethod]
+        public void AttackAndHitTest()
+        {
+            Animal a1 = new Animal() { Id = Guid.NewGuid(), Level = 5, Strength = 40, Speed = 100, Defence = 200, Name = "Dog" };
+            Animal a2 = new Animal() { Id = Guid.NewGuid(), Level = 100, Health = 200, Strength = 219, Speed = 100, Defence = 36, Name = "Cat" };
+            Attack attack = new Attack() { Damage = 80, Accuracy = 100, CriticalHitChance = 10 };
+
+            var result = a1.Attack(attack, a2);
+
+            Assert.IsTrue(result >= 0);
+        }
+    }
+}
--- Holoholona/Models/Animal.cs
+++ Holoholona/Models/Animal.cs
@@ -18,14 +18,20 @@ namespace Holoholona.Web.Models
         public int Defence { get; set; }
         public int Strength { get; set; }
 
-        public int Attack(Attack attack)
+        public int Attack(Attack attack, Animal opponent)
         {
-            return Strength * (attack.Damage / 100);
+            Random rand = new Random();
+
+            //((((2 * Level / 5 + 2) * AttackStat * AttackPower / DefenseStat) / 50) + 2) * STAB * Weakness / Resistance * RandomNumber / 100
+            return ((((2 * Level / 5 + 2) * Strength * attack.Damage / opponent.Defence) / 50) + 2) * rand.Next(100) / 100 * (rand.Next(100) <= attack.CriticalHitChance ? 2 : 1);
         }
 
         public void Hit(int dmg)
         {
-
+            Health -= dmg;
+            if (Health < 0)
+                Health = 0;
+            Console.WriteLine("Damage: " + dmg.ToString());
         }
     }
 }
